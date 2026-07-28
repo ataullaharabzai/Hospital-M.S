2893f19d01023e27dc77c55d69ba9ef0e9ba6376
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-07-28 14:28:06 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { House, User, Calendar, Stethoscope } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hello Afghanistan <House className="" size={24} />
+      </h1>
+    </>
+  );
+}
+
+export default App;
