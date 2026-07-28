9a9ad6bac20be7c297cb90d0ff741aa77af15617
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-07-28 16:23:48 +0430

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>
-        Hello Afghanistan <House className="" size={24} />
+        Hello Afghanistan <User className="p-2 rounded-full m-5 bg-blue-500 text-gray-100" size={44} />
       </h1>
     </>
   );
