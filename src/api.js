d194bbf067fec4e59f1000ee568e66db0b91a6cb
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-08-08 13:35:08 +0430

    url changed to local data

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
-export const API_URL = "https://6a76dd7c63e9caf860c325c1.mockapi.io/api/v1";
+export const API_URL = "/data";
 
 export const getDoctors = () => {
-  return fetch(`${API_URL}/doctors`).then((res) => res.json());
+  return fetch(`${API_URL}/doctors.json`).then((res) => res.json());
 };
 
 export const getPatients = () => {
-  return fetch(`${API_URL}/patients`).then((res) => res.json());
+  return fetch(`${API_URL}/patients.json`).then((res) => res.json());
 };
 
 export const getAppointments = () => {
-  return fetch(`${API_URL}/appointments`).then((res) => res.json());
+  return fetch(`${API_URL}/appointments.json`).then((res) => res.json());
 };
 
 export const getNurses = () => {
-  return fetch(`${API_URL}/nurses`).then((res) => res.json());
+  return fetch(`${API_URL}/nurses.json`).then((res) => res.json());
 };
