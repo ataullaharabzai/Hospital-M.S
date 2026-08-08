da8067cbb03b4a487cceb0aeb9aaabf46eb2bd7d
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-08-08 13:36:00 +0430

    mock api url added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
     e.preventDefault();
 
     const response = await fetch(
-      `http://localhost:3000/users?email=${encodeURIComponent(formData.email)}`,
+      `https://6a76dd7c63e9caf860c325c1.mockapi.io/api/v1/users?email=${encodeURIComponent(formData.email)}`,
     );
 
     if (!response.ok) {
