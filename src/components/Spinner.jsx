5d90538cf17fa1cfdb69fb6bac8453cabcaf7ed2
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-08-25 14:45:58 +0430

    loader bug fixed

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import {Loader2} from 'lucide-react'
+import React from "react";
+import { Loader2 } from "lucide-react";
 
 function Spinner() {
   return (
-    <div className='w-full h-screen'>
-      <Loader2 size={`40`} className='animate-spin' />
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <Loader2 className="animate-spin w-10 h-10 md:w-12 md:h-12 text-gray-500" />
     </div>
-  )
+  );
 }
 
-export default Spinner
+export default Spinner;
