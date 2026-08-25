6df64dea69971cda66dc23768edb3cca4305e397
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-08-25 13:33:36 +0430

    doctors listed

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Avatar from "../components/Avatar";
 
-function Doctor() {
+function Doctor({ name, profession, available, fee, IconT, IconB, avatar }) {
   return (
-    <div>Doctor</div>
-  )
+    <div className="border border-gray-200 shadow shadow-gray-200 p-3 rounded-md flex items-center gap-2 bg-white">
+      <div>
+        <img
+          src={avatar}
+          alt={name}
+          className="object-cover w-15 h-15 md:w-20 md:h-20 rounded-lg"
+        />
+      </div>
+      <div className="flex flex-col items-stretch gap-2 w-full">
+        <div className="w-full">
+          <div className="flex justify-between items-center gap-1 w-full">
+            <p className="text-[12px] md:text-[14px] font-semibold">{name}</p>
+            <div className="p-1 border-2 border-gray-200 rounded-lg flex items-center justify-center text-gray-500">
+              <IconT size="12" />
+            </div>
+          </div>
+          <p className="text-[11px] md:text-[12px] text-slate-500">
+            {profession}
+          </p>
+        </div>
+        <p className="text-[11px] md:text-[12px] text-slate-500">
+          Available: {available}
+        </p>
+        <div className="flex items-center justify-between w-full">
+          <p className="text-[11px] md:text-[12px] text-slate-500">
+            Starts From: {fee}
+          </p>
+          <div className="p-1 border-2 border-gray-200 rounded-lg flex items-center justify-center text-gray-500">
+            <IconB size="12" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Doctor
+export default Doctor;
--- src/pages/doctors/Grid_doctors.jsx
+++ src/pages/doctors/Grid_doctors.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getDoctors } from "../../api";
+import { NavLink } from "react-router-dom";
+import Doctor from "../../components/Doctor";
+import { ClipboardClock, EllipsisVerticalIcon, User, UserCog2Icon } from "lucide-react";
 
 function Grid_doctors() {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    getDoctors().then(setDoctors);
+  }, []);
+
   return (
-    <div>Grid_doctors</div>
-  )
+    <main>
+      <section>
+        <div className="w-full flex justify-between items-center py-2">
+          <h1 className="text-[18px] font-semibold md:text-xl">Doctor Grid</h1>
+          <p className="p-1 bg-blue-50 text-[14px] border border-blue-500 text-blue-500 rounded-lg">Total Doctors: {doctors.length}</p>
+        </div>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {doctors.map((doctor) => (
+            <div key={doctor.id}>
+              <NavLink to="/">
+                <Doctor
+                  name={doctor.name}
+                  profession={doctor.specialization}
+                  available={doctor.availableDate}
+                  fee={doctor.startFee}
+                  IconT={EllipsisVerticalIcon}
+                  IconB={ClipboardClock}
+                  avatar={doctor.image}
+                />
+              </NavLink>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default Grid_doctors
+export default Grid_doctors;
