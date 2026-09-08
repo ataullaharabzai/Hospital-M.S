ed61d5a4a88d67fa1dd1883541635682f2450c90
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-09-08 12:36:13 +0430

    changes applied

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 
-function Input({ className, type, value, onChange, name, placeholder }) {
+function Input({
+  className = "",
+  value,
+  onChange,
+  placeholder,
+  type,
+  ...props
+}) {
   return (
-    <div>
-      <input
-        placeholder={placeholder} 
-        type={type}
-        value={value}
-        onChange={onChange}
-        className={`w-full rounded-lg border border-gray-200 bg-white px-3 py-2.5 text-sm text-gray-800 outline-none transition
-            placeholder:text-gray-400
-            hover:border-gray-300
-            focus:border-blue-500 focus:ring-2 focus:ring-blue-500/10
-            dark:border-gray-700  dark:text-gray-800
-            dark:placeholder:text-gray-500
-            dark:hover:border-gray-600
-            dark:focus:border-blue-500 ${className}`}
-        name={name}
-      />
-    </div>
+    <input
+      {...props}
+      className={`${className} rounded-lg border border-gray-200 bg-white px-3 py-2.5 text-sm text-gray-800 outline-none transition placeholder:text-gray-400 hover:border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/10 dark:border-slate-700 dark:bg-slate-800/60 dark:text-slate-100 dark:placeholder:text-slate-400 dark:hover:border-slate-600 dark:focus:border-blue-500`}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      type={type}
+    />
   );
 }
 
